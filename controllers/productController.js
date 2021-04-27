a816bfc52d5a6b9101c80ddd5cb7fc468045ca05
Lew Andrade <andrade.lew@gmail.com>
2021-04-27 16:18:46 -0300

    Get All Products

---
+++ controllers/productController.js
@@ -0,0 +1,13 @@
+const Product = require("../models/productModel");
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+module.exports = { getProducts };
